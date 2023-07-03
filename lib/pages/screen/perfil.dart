65be85391e0e25a93e1d43ca4660663ffce83bbb
ANGELES NAGELY QUINATOA SANCHEZ <97192998+AngelesQuinatoa@users.noreply.github.com>
2023-07-03 17:19:28 -0500

    Perfil

--- lib/pages/screen/perfil.dart
+++ lib/pages/screen/perfil.dart
@@ -1,76 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:project/pages/screen/cart.screen.dart';
 import '../components/drawer.dart';
 import '../components/navbar.dart';
 
 
 class UserProfileScreen extends StatelessWidget {
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(
-          'Mi Perfil',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+        title: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Yavi',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Lobster',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Food',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 232, 153, 88),
+                  fontFamily: 'Lobster',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
         iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+            icon: Stack(
+              children: [
+                Icon(
+                  Icons.shopping_cart,
+                  color: Color.fromARGB(255, 232, 153, 88),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 16,
+                      minHeight: 16,
+                    ),
+                    child: Text(
+                      CartScreen.cartItems.length.toString(),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 232, 153, 88),
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CartScreen(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       drawer: DrawerNav(),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Card(
-            elevation: 5,
-            shadowColor: Colors.grey[400],
+      body: Stack(
+        children: [
+          Container(
+            color: const Color.fromARGB(255, 249, 249, 249).withOpacity(0.2), // Color de fondo plomo con poca opacidad
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
             child: Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 80,
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'Jinson',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Correo electrónico: ',
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Ubicación: ',
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Acción para editar el perfil del usuario
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                    ),
-                    child: Text('Editar perfil'),
+              height: 160, // Altura de la portada
+              color: Colors.grey[300], // Color de la portada
+            ),
+          ),
+          Positioned(
+            top: 120,
+            left: (MediaQuery.of(context).size.width - 120) / 2,
+            child: Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 179, 177, 177),
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
                 ],
               ),
+              child: ClipOval(
+                child: Image.asset(
+                  '../images/perfil.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
-        ),
+          Positioned(
+            top: 250,
+            left: 16,
+            right: 16,
+            bottom: 0, // Ajuste para expandir la tarjeta hasta abajo
+            child: Card(
+              elevation: 5,
+              shadowColor: Colors.black.withOpacity(0.9), // Sombra de color negro bajito
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Carlos Alesandro Flores Téran',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Nombre: Jinson',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Correo electrónico: jinson@example.com',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Ubicación: Ciudad XYZ',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black,
+                        ),
+                        onPressed: () {
+                          // Acción al presionar el botón
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.exit_to_app,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              'Salir',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigation(),
-    );
-  }
+    ),
+  );
+}
 }
